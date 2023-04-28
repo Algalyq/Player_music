527e7a2b6153f8d20f2994762ab28f23b0b4f86f
Algalyq <algalyq@mail.ru>
2023-04-28 14:46:33 +0600

    Adding swagger

--- back/Music_app/Music_app/urls.py
+++ back/Music_app/Music_app/urls.py
@@ -3,11 +3,31 @@ from django.urls import path,include
 from django.contrib.staticfiles.urls import static
 from rest_framework_simplejwt import views as jwt_views
 from django.conf import settings
+from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="API Documentation",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('music/', include('Music.urls')),
     path('user/', include('Auth.urls')),
-
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+  
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
